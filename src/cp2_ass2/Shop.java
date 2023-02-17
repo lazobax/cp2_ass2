02426bba3c5de57075a8631c22e11e4618d70982
unknown <lazo.ali@lau.edu>
2023-02-17 21:51:08 +0200

    added addVehicle() and displayType() methods

--- src/cp2_ass2/Shop.java
+++ src/cp2_ass2/Shop.java
@@ -1,5 +1,195 @@
 package cp2_ass2;
 
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
 public class Shop {
+	
+	public ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+	public String store_name;
+	
+	public Shop(String n) {
+		store_name = n;
+	}
+	
+	public void displayType(int type) {
+		
+		int num = 1;
+		switch(type) {
+		case 1:
+			
+			for(int i=0; i<vehicles.size(); i++) {
+				if(vehicles.get(i) instanceof Car) {
+					String vhc = num + ":    "+ vehicles.get(i).toString();
+					System.out.println(vhc);
+					num ++;
+				}
+				
+				
+			}
+			
+			break;
+
+			
+		case 2:
+			
+			for(int i=0; i<vehicles.size(); i++) {
+				if(vehicles.get(i) instanceof Truck) {
+					String vhc = num + vehicles.get(i).toString();
+					System.out.println(vhc);
+					num ++;
+				}		
+				
+				
+			}
+			
+			break;
+			
+		case 3:
+			
+			for(int i=0; i<vehicles.size(); i++) {
+				if(vehicles.get(i) instanceof Motorcycle) {
+					String vhc = num + vehicles.get(i).toString();
+					System.out.println(vhc);
+					num ++;
+				}
+				
+				
+			}
+			
+			break;
+
+		}
+		
+			}
+		
+		
+	
+	
+	public void addVehicle() {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("_________________");
+		System.out.println("Add Vehicle");
+		System.out.println("_________________");
+		
+		System.out.println("What is your vehicle Type?");
+		System.out.println("1: Car    2: Truck    3: Motorcycle");
+		
+		int vehicle_type = input.nextInt();
+		input.nextLine();
+		
+		System.out.println("What is the Brand Name? ");
+		String bn = input.nextLine();
+		
+		System.out.println("What is the Date of Make?");
+		String dom = input.nextLine();
+		
+		System.out.println("What is the color?");
+		String col = input.nextLine();
+		
+		switch(vehicle_type) {
+		
+		case 1:
+			
+			System.out.println("What is the Seat Number?");
+			int sn = input.nextInt();
+			
+			String[] car_types = {"", "Sedan", "SUV", "Hatchback"};
+			System.out.println("What is the car type?");
+			System.out.println("1: Sedan    2: SUV    3: Hatchback");
+			int car_type = input.nextInt();
+			
+			Car c = new Car(bn, dom, col, car_types[car_type], sn);
+			
+			vehicles.add(c);
+			
+			break;
+		
+		case 2:
+			
+			System.out.println("What is the Wheel Number?");
+			int wn = input.nextInt();
+			
+			Truck t = new Truck(bn, dom, col, wn);
+			
+			vehicles.add(t);
+			
+			break;
+		
+		case 3:
+			
+			System.out.println("What is the engine capacity?");
+			float ec = input.nextFloat();
+			
+			Motorcycle m = new Motorcycle(bn, dom, col, ec);
+			
+			vehicles.add(m);
+			
+			break;
+		default:
+			System.out.println("You chose an unavailable vehicle type!");
+		}
+		
+		System.out.println("Vehicle added successfully!");
+	
+		
+	}
+	
+	public void deleteVehicle() {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("_________________");
+		System.out.println("Add Vehicle");
+		System.out.println("_________________");
+		
+		System.out.println("What is your vehicle Type?");
+		System.out.println("1: Car    2: Truck    3: Motorcycle");
+		
+		int vehicle_type = input.nextInt();
+		input.nextLine();
+		
+		switch(vehicle_type) {
+		case 1:
+			
+			
+		case 2:
+			
+		case 3:
+			
+		default:
+			
+		}
+	}
+	
+	public void displayType() {
+		
+	}
+	
+	public void listAll() {
+		
+	}
+	
+	public void menu() {
+		
+	}
+	
+	public static void main(String[] concs) {
+		
+		Shop s = new Shop("poop");
+		
+		for(int i = 0; i<6 ; i++ ) {
+			s.addVehicle(); 
+		}
+		
+		s.displayType(1);
+		s.displayType(2);
+		s.displayType(3);
+		
+		
+	}
 
 }
